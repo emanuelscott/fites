e83d1cfffe7f01eae71df8b8b55fadfe8fba3a1f
smart fix <smartfix@smarts-MacBook-Pro.local>
2025-01-19 20:57:03 +0500

    feat(ui): update meta-tags add logo, update favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,15 +10,10 @@ export const metadata: Metadata = {
     "Are you tired of feeling stuck, overwhelmed, or like there’s never enough time to focus on yourself? Emanuel Scott Fitness helps you break free, rebuild your confidence, and take control of your body, mind, and future. No matter how busy life gets, imagine waking up stronger, more focused, and ready to thrive. Your transformation starts now—because you’re worth it!",
   icons: {
     icon: [
-      { url: "./meta-logo.jpg", sizes: "16x16", type: "image/jpg" },
-      { url: "./meta-logo.jpg", sizes: "any", type: "image/jpg" },
+      { url: "https://www.esfitness.io/preview.png", sizes: "16x16", type: "image/jpg" },
+      { url: "https://www.esfitness.io/preview.png", sizes: "any", type: "image/jpg" },
     ],
   },
-
-  // icons: [
-  //   { rel: 'icon', url: '/meta-logo.jpg' },
-  // ],
-
   openGraph: {
     title: "ESFitness",
     description:
@@ -26,7 +21,7 @@ export const metadata: Metadata = {
     url: "https://www.esfitness.io",
     images: [
       {
-        url: "./preview.png",
+        url: "https://www.esfitness.io/preview.png",
         width: 1200,
         height: 630,
         alt: "ESFitness",
